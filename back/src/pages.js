78b620ba8df0799b82e2eb074af8c62382596382
furtivusMaj <ahmed.mejb@gmail.com>
2019-10-04 16:09:14 +0300

    refactore createSession()

--- back/src/pages.js
+++ back/src/pages.js
@@ -1,41 +1,35 @@
 const axios = require("axios");
-const loginURL = "https://users.premierleague.com/accounts/login/";
-const axiosInstace = axios.create({ baseURL: loginURL });
+const axiosInstance = axios.create({
+  baseURL: `https://fantasy.premierleague.com/api/leagues-classic/`
+});
 
 //set cookie manually, then attach it to axios's headers
-const createSession = async () => {
-  //manually setting the cookie
+const createSession = () => {
   const cookie =
     "pl_profile=eyJzIjogIld6SXNORFUwTVRNNU1qSmQ6MWkyZlJPOlFUQ3djbGp2MllUNnMwck5DbWg3NVFiUEhpZyIsICJ1IjogeyJpZCI6IDQ1NDEzOTIyLCAiZm4iOiAiQWhtZWQiLCAibG4iOiAiTWVqYnJpIiwgImZjIjogMX19; expires=Tue, 25 Aug 2020 17:38:58 GMT; Max-Age=31449600 Path=/; SameSite=";
-  axiosInstace.defaults.headers.Cookie = cookie;
+  axiosInstance.defaults.headers.Cookie = cookie;
 };
 
-const getPages = leagueId => {
-  return new Promise(resolve => {
-    createSession().then(async () => {
-      //1st request is always made
-      let page = 1;
-      firstRes = await axiosInstace.get(
-        `https://fantasy.premierleague.com/api/leagues-classic/${leagueId}/standings/?page_new_entries=1&page_standings=${page}&phase=1`
-      );
-      standings = await firstRes.data.standings;
-      results = await standings.results;
+const getPages = async leagueId => {
+  createSession();
 
-      while (results.length !== 0) {
-        //make the next request
-        console.log("reading page : ", page);
-        page++;
+  let page = 1;
+  firstRes = await axiosInstance.get(
+    `https://fantasy.premierleague.com/api/leagues-classic/${leagueId}/standings/?page_new_entries=1&page_standings=${page}&phase=1`
+  );
+  standings = await firstRes.data.standings;
+  results = await standings.results;
 
-        res = await axiosInstace.get(
-          `https://fantasy.premierleague.com/api/leagues-classic/${leagueId}/standings/?page_new_entries=1&page_standings=${page}&phase=1`
-        );
-        standings = await res.data.standings;
-        results = await standings.results;
-      }
-      console.log(page);
-      resolve(page.toString());
-    });
-  });
+  while (results.length !== 0) {
+    //make the next request
+    page++;
+    res = await axiosInstance.get(
+      `https://fantasy.premierleague.com/api/leagues-classic/${leagueId}/standings/?page_new_entries=1&page_standings=${page}&phase=1`
+    );
+    standings = await res.data.standings;
+    results = await standings.results;
+  }
+  return page.toString();
 };
 
 module.exports = getPages;
