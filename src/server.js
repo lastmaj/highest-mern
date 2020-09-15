2534e6085b3121de9049090049afd30279af442d
furtivusMaj <ahmed.mejb@gmail.com>
2020-09-15 15:17:02 +0100

    heroku deploy fix

--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const highest = require("./highest.js");
-const pages = require("./pages.js");
-const cors = require("cors");
+const express = require('express');
+const highest = require('./highest.js');
+const pages = require('./pages.js');
+const cors = require('cors');
 
 const app = express();
 app.use(cors());
 
 //landing page
-app.get("/", (_, res) => {
-  res.send("Hello World");
+app.get('/', (_, res) => {
+  res.send('Hello World');
 });
 
 //request number of pages in a league
@@ -23,9 +23,9 @@ app.get("/", (_, res) => {
 //request league page
 //2nd way to error handle; try catch blocks
 //could have used the default express error handler (ex: /:id route)
-app.get("/:id", async (req, res) => {
+app.get('/:id', async (req, res) => {
   req.setTimeout(0);
-  res.header("Content-Type", "application/json");
+  res.header('Content-Type', 'application/json');
   try {
     const result = await highest(req.params.id);
     res.send(JSON.stringify(result));
@@ -35,7 +35,7 @@ app.get("/:id", async (req, res) => {
 });
 
 //capture the annoying favicon.ico request
-app.get("/favicon.ico", (_, res) => res.status(204));
+app.get('/favicon.ico', (_, res) => res.status(204));
 
 //error handler
 app.use((err, req, res, next) => {
@@ -46,18 +46,18 @@ app.use((err, req, res, next) => {
 //uses the express static middleware to grab the build folder
 //when react is built, the app is going to be in the indicated folder
 
-if (process.env.NODE_EVN === "production") {
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 
   //SPA: always return the index.html
   //check what the first part means though
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-  console.log("server is running on 4000");
+  console.log('server is running on 4000');
 });
