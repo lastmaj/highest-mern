2067c559e8348905b03181e02c47d6fa65ed69e8
furtivusMaj <ahmed.mejb@gmail.com>
2020-09-16 18:12:05 +0100

    return top 3 players instead of one

--- src/highest.js
+++ src/highest.js
@@ -13,17 +13,8 @@ Object.defineProperty(Array.prototype, 'flat', {
   },
 });
 
-//get the max standing from a standings page
-const getMaxFromPage = (results) => {
-  const maxPoints = results.reduce((prev, curr) =>
-    prev.event_total > curr.event_total ? prev : curr
-  )['event_total'];
-  return results.filter((r) => r.event_total === maxPoints);
-};
-
 //post a request for standings then return the max on that page
 const getMax = async (leagueId) => {
-  //createSession();
   //initialization
   let page = 1;
   let maxs = [];
@@ -33,34 +24,55 @@ const getMax = async (leagueId) => {
     url: `https://fantasy.premierleague.com/api/leagues-classic/${leagueId}/standings/?page_new_entries=1&page_standings=${page}&phase=1`,
     json: true,
   };
+
   //1st request is always made
   firstRes = await rp(options);
-
   let results = firstRes.standings.results;
+  let scores = [],
+    winners = [];
 
   //if there are results, carry on
   while (results.length !== 0) {
+    //1st page : push first 3 scores
+    scores.push(
+      results.reduce((curr, next) =>
+        curr.event_total > next.event_total ? curr : next
+      ).event_total
+    );
+    //push second higher score
+    scores.push(
+      results.reduce((curr, next) =>
+        curr.event_total > next.event_total && curr.event_total !== scores[0]
+          ? curr
+          : next
+      ).event_total
+    );
+    //push third highest score
+    scores.push(
+      results.reduce((curr, next) =>
+        curr.event_total > next.event_total &&
+        !scores.includes(curr.event_total)
+          ? curr
+          : next
+      ).event_total
+    );
+
+    //concat the three winners to winners, sort and take first three
+    winners = winners.concat(
+      results.filter((x) => scores.includes(x.event_total))
+    );
+
+    //if we have more than one page (more than 3 winners), sort and take first 3 winners
+    if (winners.length > 3) {
+      winners.sort((a, b) => b.event_total - a.event_total).splice(3);
+    }
     console.log(`Reading page no: ${page}`);
-    let pageMax = getMaxFromPage(results);
-    maxs.push(pageMax);
 
     //make the next request
     options.url = `https://fantasy.premierleague.com/api/leagues-classic/${leagueId}/standings/?page_new_entries=1&page_standings=${++page}&phase=1`;
     res = await rp(options);
     standings = res.standings;
     results = standings.results;
-  }
-
-  //helper array
-  const winners = [];
-  const helperArray = maxs.map((x) => x[0]['event_total']);
-  const globalMax = Math.max(...helperArray);
-
-  //iterate through maxs and helperArray
-  let index = helperArray.indexOf(globalMax);
-  while (index !== -1) {
-    winners.push(maxs[index]);
-    index = helperArray.indexOf(globalMax, index + 1);
   }
 
   //display time
@@ -77,7 +89,10 @@ const getMax = async (leagueId) => {
     execution_time: time / 1000 + ' s',
     league_total_pages: page - 1,
   };
+  console.log(result);
   return result;
 };
+
+getMax(635004);
 
 module.exports = getMax;
